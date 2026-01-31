62757f2f62b5df0da0d1c3a46ef4001cea126f81
youssef <youssef24p@gmail.com>
2026-01-31 23:05:24 +0200

    .

--- cmd/main.go
+++ cmd/main.go
@@ -1,34 +1,5 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"math/rand"
-)
-
-type QueryError struct {
-	Message string
-}
-
-func (e QueryError) Error() string { return e.Message }
-
-func executeQuery() (int, error) {
-	randomBit := rand.Intn(2)
-	if randomBit == 1 {
-		return 0, errors.New("Unexpected error")
-	} else {
-		return 0, &QueryError{Message: "socket not found"}
-	}
-
-}
-
 func main() {
-	result, err := executeQuery()
-	if e, ok := err.(*QueryError); ok {
-		fmt.Println("QueryError:", e.Message)
-	} else if err != nil {
-		fmt.Println("An unexpected error occurred:", err)
-	} else {
-		fmt.Println("Query executed successfully, result:", result)
-	}
+
 }
